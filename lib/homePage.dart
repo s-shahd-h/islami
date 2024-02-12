57e20851b28560925f8636ffd2567b77f43c3c1f
s-shahd-h <shahd.hussien14@gmail.com>
2024-02-12 22:29:26 +0200

    init app

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -5,7 +5,10 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+appBar: AppBar(
+  backgroundColor: Colors.cyan,
+  title:Text("Islami") ,
+),
     );
   }
 }
