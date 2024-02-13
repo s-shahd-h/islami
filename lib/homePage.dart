fc5b1152af01a800fcce6311542a16b76a17b985
s-shahd-h <shahd.hussien14@gmail.com>
2024-02-13 02:34:52 +0200

    quran screen

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
-class Home extends StatelessWidget {
-  const Home({super.key});
+
+class QuranPage extends StatelessWidget {
+  const QuranPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-  backgroundColor: Colors.cyan,
-  title:Text("Islami" ) ,
-),
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          "اسلامي",
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/pg3.png'),
+          fit: BoxFit.cover,
+        )),
+      ),
     );
   }
 }
